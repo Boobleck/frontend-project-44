805f5257abfa5bb732776e391d9e400bc2198b27
Артёмус <artem_sherstnev_2000@mail.ru>
2026-04-06 13:00:18 +0700

    final?

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,8 +3,39 @@ import readlineSync from 'readline-sync';
 const name = BrainGame();
 export function BrainEven() {
     console.log('Question: 15')
-    const answer = readlineSync.question('Your answer is: ');
-    if (answer == "No", "no") console.log("Correct!");
-    else console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`),
-        console.log(`Let's try again, ${name}`)
+    const answer = readlineSync.question('Your answer is: ') .toLowerCase();
+    if (answer === "no") {
+        console.log("Correct!");
+    }
+    else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`),
+        console.log(`Let's try again, ${name}`);
+        return false;
+    }
 }
+export function BrainEvenRaundTwo() {
+    console.log('Question: 6')
+    const answer = readlineSync.question('Your answer is: ') .toLowerCase();
+    if (answer === "yes"){ 
+        console.log("Correct!");
+    }
+    else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`),
+        console.log(`Let's try again, ${name}`);
+        return false;
+    }
+}
+export function BrainEvenRaundThree() {
+    console.log('Question: 7')
+    const answer = readlineSync.question('Your answer is: ') .toLowerCase();
+    if (answer === "no") {
+        console.log("Correct!");
+        console.log(`Congratulations, ${name}`)
+        return false;
+    }
+    else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`),
+        console.log(`Let's try again, ${name}`);
+        return false;
+    }
+}   
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 import {BrainGame} from '../src/cli.js';
 // BrainGame();
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`)
+// console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
 import {BrainEven} from './brain-even.js';
-BrainEven();
+import {BrainEvenRaundTwo} from './brain-even.js';
+import {BrainEvenRaundThree} from './brain-even.js';
+function play() {
+if (BrainEven()=== false) return;
+if (BrainEvenRaundTwo()=== false) return;
+if (BrainEvenRaundThree()=== false) return;
+}
+play();
