5b0607c77eb3e75f286df41a330b20c57d84834f
Boobleck <artgol2007@gmail.com>
2026-04-09 00:28:10 +0700

    09.04.26

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import {BrainGame} from '../src/cli.js';
-// BrainGame();
-// console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
-import {BrainEven} from './brain-even.js';
-import {BrainEvenRaundTwo} from './brain-even.js';
-import {BrainEvenRaundThree} from './brain-even.js';
+BrainGame();
+
+import {BrainEven, BrainEvenRaundTwo, BrainEvenRaundThree} from './brain-even.js';
 function play() {
 if (BrainEven()=== false) return;
 if (BrainEvenRaundTwo()=== false) return;
